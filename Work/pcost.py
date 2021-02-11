2d21ba81bd24bbddef74c6ea9596133eeb36871e
muriithi-ian <imuriithiian@gmail.com>
2021-02-11 14:01:45 +0300

    Working with files

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Work\Data\portfolio.csv', 'rt') as f:
+    next(f)
+    total = 0
+    for line in f:
+        row = line.split(',')
+        shares = int(row[1])
+        price = float(row[2])
+        total += shares * price
+
+print( total)
